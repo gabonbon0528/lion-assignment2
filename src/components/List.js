bd90395e3a8b939948276f870d4502d4019a0d7f
gabonbon <judy860528@gmail.com>
2023-08-07 17:50:58 +0800

    add components

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+function List() {
+  return (
+    <div className="list">
+      <div className="title">遊玩交通</div>
+      <div className="tags">
+        <div className="tag">鐵路</div>
+        <div className="tag">巴士</div>
+        <div className="tag">郵輪</div>
+      </div>
+      <span className="border"></span>
+      <div className="switch-btn">更多</div>
+    </div>
+  );
+}
+
+export default List;
