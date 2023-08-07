4710e555b15656c016aaf73498c3d6efceea8068
gabonbon0528 <judy860528@gmail.com>
2023-08-07 23:15:15 +0800

    travel type

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import List from "./components/List";
-import TripTypeList from '../src/assests/TripTypeList.json'
+import TripTypeList from "../src/assests/TripTypeList.json";
+import CategoryList from "../src/assests/CategoryList.json";
 
 function App() {
-  console.log(TripTypeList);
-  const data = TripTypeList;
+  const dataTripTypeList = TripTypeList;
+  const dataCAtegoryList = CategoryList;
+
+  // console.log(TripTypeList);
+  // console.log(CategoryList);
 
   return (
     <div className="wrapper">
-      <List />
+      <List tripType={TripTypeList} />
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,20 @@
-function List() {
+import Tag from "../components/Tag";
+
+function List(tripTypes) {
+  console.log(tripTypes.tripType);
+
   return (
     <div className="list">
       <div className="title">遊玩交通</div>
       <div className="tags">
-        <div className="tag">鐵路</div>
+
+        {tripTypes.tripType.map((trip) => {
+          return <div className="tag" key={trip.TypeCode}>{trip.TypeName}</div>;
+        })}
+
+        {/* <div className="tag">鐵路</div>
         <div className="tag">巴士</div>
-        <div className="tag">郵輪</div>
+        <div className="tag">郵輪</div> */}
       </div>
       <span className="border"></span>
       <div className="switch-btn">更多</div>
